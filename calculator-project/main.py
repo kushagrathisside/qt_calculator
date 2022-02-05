f1893c52fcc4a7245cc0c3731664817bd6246086
Zhandos <zhandos.kadyrkulov@gmail.com>
2022-02-05 16:04:31 +0600

    Added arithmetic operations support

--- calculator-project/main.py
+++ calculator-project/main.py
@@ -1,16 +1,28 @@
+#Standard Modules
 import sys
 
 
+#Third-Party Modules
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (QApplication, QGridLayout, QLineEdit, 
                              QVBoxLayout, QPushButton, QWidget)
-from PyQt6.QtGui import QFont
+from PyQt6.QtGui import QFont, QIcon
 
 
 class MainWindow(QWidget):
+    """Calculator's Main Window
+
+    Args:
+        QWidget ([type]): QWidget Parent Class
+    """
     
     def __init__(self) -> None:
+        """Class Initialization
+        """
+        
         super().__init__()
+        
+        self.prev_operation = ""
         
         self._window_settings()
         
@@ -19,15 +31,25 @@ class MainWindow(QWidget):
         self.setLayout(layout)
         
     def _window_settings(self) -> None:
+        """Window Settings
+        """
+        
+        self.setWindowIcon(QIcon("icons/calculator.png"))
         self.setWindowTitle("Калькулятор Morrison")
-        self.setMinimumSize(310, 340)
+        self.setFixedSize(320, 340)
         
     def _main_layout(self) -> QVBoxLayout:
+        """Calculator's Window Layout
+
+        Returns:
+            QVBoxLayout: Vertical Layout
+        """
+        
         vbox = QVBoxLayout()
         self.text = ""
         
         self.output_line = QLineEdit(self.text)
-        self.output_line.setMinimumSize(310, 80)
+        self.output_line.setFixedSize(300, 80)
         self.output_line.setAlignment(Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignBottom)
         self.output_line.setFont(QFont("Times", 14))
         self.output_line.setReadOnly(True)
@@ -41,34 +63,29 @@ class MainWindow(QWidget):
         return vbox
         
     def _grid_layout(self) -> QGridLayout:
+        """Buttons Grid Layout
+
+        Returns:
+            QGridLayout: Grid Layout
+        """
         
         grid = QGridLayout()
         
         #Number Buttons
-        one_btn = self.numbers_buttons(1)
-        two_btn = self.numbers_buttons(2)
-        three_btn = self.numbers_buttons(3)
-        four_btn = self.numbers_buttons(4)
-        five_btn = self.numbers_buttons(5)
-        six_btn = self.numbers_buttons(6)
-        seven_btn = self.numbers_buttons(7)
-        eight_btn = self.numbers_buttons(8)
-        nine_btn = self.numbers_buttons(9)
-        zero_btn = self.numbers_buttons(0)
+        one_btn = self._numbers_btns_init(1)
+        two_btn = self._numbers_btns_init(2)
+        three_btn = self._numbers_btns_init(3)
+        four_btn = self._numbers_btns_init(4)
+        five_btn = self._numbers_btns_init(5)
+        six_btn = self._numbers_btns_init(6)
+        seven_btn = self._numbers_btns_init(7)
+        eight_btn = self._numbers_btns_init(8)
+        nine_btn = self._numbers_btns_init(9)
+        zero_btn = self._numbers_btns_init(0)
         
         #Sign Buttons
-        add_btn = QPushButton("+")
-        add_btn.setMinimumSize(72, 50)
-        sub_btn = QPushButton("-")
-        sub_btn.setMinimumSize(72, 50)
-        mult_btn = QPushButton("*")
-        mult_btn.setMinimumSize(72, 50)
-        div_btn = QPushButton("/")
-        div_btn.setMinimumSize(72, 50)
-        equal_btn = QPushButton("=")
-        equal_btn.setMinimumSize(72, 50)
-        clear_btn = QPushButton("C")
-        clear_btn.setMinimumSize(72, 50)
+        add_btn, sub_btn, mult_btn, \
+            div_btn, equal_btn, clear_btn = self._sign_btns_init()
         
         #Adding Buttons to a Grid
         buttons = [one_btn, two_btn, three_btn, add_btn,
@@ -83,25 +100,133 @@ class MainWindow(QWidget):
             count += 3
         
         return grid
+
+    def _sign_btns_init(self) -> QPushButton:
+        """Creating buttons for signs
+
+        Returns:
+            QPushButton: Buttons
+        """
+        
+        add_btn = self._op_btns_init("+")
+        sub_btn = self._op_btns_init("-")
+        mult_btn = self._op_btns_init("*")
+        div_btn = self._op_btns_init("/")  
+        equal_btn = self._op_btns_init("=")
+        
+        clear_btn = QPushButton("C")
+        clear_btn.setMinimumSize(75, 50)
+        clear_btn.clicked.connect(self._clear_text)
+        
+        return add_btn, sub_btn, mult_btn, div_btn, equal_btn, clear_btn
+
+    def _op_btns_init(self, operation: str) -> QPushButton:
+        """Handles operation buttons
+
+        Args:
+            operation (str): Operation sign
+
+        Returns:
+            QPushButton: Operation button
+        """
+        
+        op_btn = QPushButton(operation)
+        op_btn.setMinimumSize(75, 50)
+        op_btn.clicked.connect(lambda: self._operation(op_btn.text()))
+        
+        return op_btn
     
-    def numbers_buttons(self, number: int) -> QPushButton:
+    def _numbers_btns_init(self, number: int) -> QPushButton:
+        """Handles number buttons creation
+
+        Args:
+            number (int): Number from 0 to 9
+
+        Returns:
+            QPushButton: Button
+        """
         
         num_btn = QPushButton(str(number))
-        num_btn.setMinimumSize(72, 50)
-        num_btn.clicked.connect(lambda: self.add_numbers(num_btn.text()))
+        num_btn.setMinimumSize(75, 50)
+        num_btn.clicked.connect(lambda: self._add_numbers(num_btn.text()))
         
         return num_btn
     
-    def add_numbers(self, number: str) -> None:
+    def _add_numbers(self, number: str) -> None:
+        """Adding numbers to the calculator screen
+
+        Args:
+            number (str): Number button text
+        """
         
-        self.text += number
+        if self.prev_operation == "=":
+            self.text = ""
+            self.prev_operation = ""
+            self.text += number
+        else:
+            self.text += number     
+            if self.text[0] == "0":
+                self.text = self.text[1:]
+        
         self.output_line.setText(self.text)
         
+    def _operation(self, operation: str) -> None:
+        """Handles arithmetic operations
+
+        Args:
+            operation (str): Operation button text
+        """
+        
+        res = 0
+        
+        if operation == "+":
+            self.first_value = int(self.output_line.text())
+            self.prev_operation = operation
+            self.text = ""
+        if operation == "-":
+            self.first_value = int(self.output_line.text())
+            self.prev_operation = operation
+            self.text = ""
+        if operation == "*":
+            self.first_value = int(self.output_line.text())
+            self.prev_operation = operation
+            self.text = ""
+        if operation == "/":
+            self.first_value = int(self.output_line.text())
+            self.prev_operation = operation
+            self.text = ""
+        if operation == "=":
+            if self.prev_operation == "+":
+                self.prev_operation = operation
+                res = self.first_value + int(self.output_line.text())
+                self.text = str(res)
+                self.output_line.setText(self.text)
+            if self.prev_operation == "-":
+                self.prev_operation = operation
+                res = self.first_value - int(self.output_line.text())
+                self.text = str(res)
+                self.output_line.setText(self.text)
+            if self.prev_operation == "*":
+                self.prev_operation = operation
+                res = self.first_value * int(self.output_line.text())
+                self.text = str(res)
+                self.output_line.setText(self.text)
+            if self.prev_operation == "/":
+                self.prev_operation = operation
+                res = self.first_value / int(self.output_line.text())
+                self.text = str(res)
+                self.output_line.setText(self.text)
+            
+    def _clear_text(self) -> None:
+        """Clears calculator screen
+        """
+        
+        self.text = ""
+        self.output_line.setText(self.text)
         
 
-app = QApplication(sys.argv)
-main_window = MainWindow()
-main_window.show()
-
 if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()  
     sys.exit(app.exec())
